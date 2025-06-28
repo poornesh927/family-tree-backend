f0ad85ffe7b42f98a5eef7dd3543c84f6eab4c18
Poornesh Mangapuram <poornesh806@gmail.com>
2025-06-28 23:22:30 +0530

    m

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers } = require('../controllers/adminController'); // create next
-const { verifyAdmin } = require('../middleware/authMiddleware'); // optional middleware
+// const { verifyAdmin } = require('../middleware/authMiddleware'); // optional middleware
 
 // GET /api/admin/users
-router.get('/users', verifyAdmin, getAllUsers); // You can remove `verifyAdmin` for now
+// router.get('/users', verifyAdmin, getAllUsers); // You can remove `verifyAdmin` for now
 
-module.exports = router;
+module.exports = router;router.get('/users', getAllUsers);
+
